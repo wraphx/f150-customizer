149be3d09449c5d4ae1e0c0229f812e5f2d0ad79
Ty Byers <tyrone@wraphx.com>
2024-09-05 11:20:25 -0400

    updated Configurator

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ export default function App() {
           <Suspense fallback={null}>
           <F150 scale={2.7} position={[0, 0, 0]} />
           </Suspense>
-          <hemisphereLight intensity={0.5} />
+          <hemisphereLight intensity={2.5} />
           <ContactShadows position={[0, 0, 0.0]} opacity={0.75} scale={15} blur={1.5} far={0.8} />
           <ContactShadows resolution={1024} position={[0, 0, 0]} scale={20} blur={0.5} opacity={0.5} far={10} />
-          <ambientLight intensity={0.7} />
+          <ambientLight intensity={2.7} />
           <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
 
           {/* <Environment preset="city" /> */}
